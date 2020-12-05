41df72441c1c99562b4c176f24ca55ffd8719207
ArnaudBacq <arnaud.bacq@student.uclouvain.be>
2020-12-05 12:34:03 +0100

    Server gère plusieurs clients

--- src/MyServer.java
+++ src/MyServer.java
@@ -2,6 +2,52 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.*;
+import java.text.SimpleDateFormat;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+
+class TaskThread implements Runnable
+{
+    private Socket clientSocket;
+
+    public TaskThread(Socket clientSocket){
+        this.clientSocket = clientSocket;
+    }
+
+    // Prints task name and sleeps for 1s
+    // This Whole process is repeated 5 times
+    public void run()
+    {
+        try
+        {
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+            String inputLine, outputLine;
+
+            // Initiate conversation with client
+            MyProtocol protocol = new MyProtocol();
+            outputLine = protocol.processInput(null);
+            out.println(outputLine);
+
+            while ((inputLine = in.readLine()) != null) {
+                outputLine = protocol.processInput(inputLine);
+                out.println(outputLine);
+                if (outputLine.equals("Bye."))
+                    break;
+            }
+        }
+
+        catch(IOException e)
+        {
+            System.out.println("this port is already taken or the client disconnected");
+        }
+    }
+}
+
+
 
 public class MyServer {
     //les données du fichier
@@ -39,26 +85,16 @@ public class MyServer {
             }
             bufferedReader.close();
 
-            //attend qu'un client essaie de se connecter
-            Socket clientSocket = serverSocket.accept();//retourne un socket lié au port portNumber et avec le n. de port et l'adresse du client
-            System.out.println("client connecte");
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
-                BufferedReader in = new BufferedReader(
-                        new InputStreamReader(clientSocket.getInputStream()));
-            String inputLine, outputLine;
-
-            // Initiate conversation with client
-            MyProtocol protocol = new MyProtocol();
-            outputLine = protocol.processInput(null);
-            out.println(outputLine);
-
-            while ((inputLine = in.readLine()) != null) {
-                outputLine = protocol.processInput(inputLine);
-                out.println(outputLine);
-                if (outputLine.equals("Bye."))
-                    break;
+            ExecutorService executor = Executors.newFixedThreadPool(2); // Le nombre maximum de threads
+            while(true){
+                //On attend qu'un client veuille se connecter
+                Socket clientSocket = serverSocket.accept();//retourne un socket lié au port portNumber et avec le n. de port et l'adresse du client
+                System.out.println("client connecte");
+                Runnable worker = new TaskThread(clientSocket);
+                executor.execute(worker);
             }
-            System.out.println("fin de try");
+
+            //System.out.println("fin de try");
         }
         catch(IOException e){
             System.out.println("this port is already taken or the client disconnected");
