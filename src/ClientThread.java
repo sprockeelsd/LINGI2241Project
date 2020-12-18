eedd4fbe639127e50d6d9f764e9044bfa408a961
Damien Sprockeels <damien.sprockeels@student.uclouvain.be>
2020-12-18 14:21:08 +0100

    séparé la classe main

---
+++ src/ClientThread.java
@@ -0,0 +1,26 @@
+//import pour la classe
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.text.SimpleDateFormat;
+class ClientThread implements Runnable{
+    private String[] Requests;
+    private int id;
+
+    public ClientThread(int id,String[] Requests){
+        this.Requests = Requests;
+        this.id = id;
+    }
+
+    public void  run(){
+        try {
+            MyClient client = new MyClient(this.id, this.Requests);
+            client.connect("LAPTOP-TQFF0SRJ", 1234);
+        }
+        catch (IOException e){
+            System.err.println("I/O Exception error");
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,6 @@ import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-
-//import pour la classe
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
-import java.net.Socket;
-import java.text.SimpleDateFormat;
-class ClientThread implements Runnable{
-    private String[] Requests;
-    private int id;
-
-    public ClientThread(int id,String[] Requests){
-        this.Requests = Requests;
-        this.id = id;
-    }
-
-    public void  run(){
-        try {
-            MyClient client = new MyClient(this.id, this.Requests);
-            client.connect("LAPTOP-I9J1EU77", 1234);
-        }
-        catch (IOException e){
-            System.err.println("I/O Exception error");
-        }
-    }
-}
-
 public class Main {
     public static void main(String[] args) throws IOException {
         //commande hostname     Damien : LAPTOP-TQFF0SRJ  Arnaud : LAPTOP-I9J1EU77
